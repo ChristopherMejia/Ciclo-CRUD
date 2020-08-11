b8e26ae1c1f067861321363f31965e06a1127bfc
Christopher Mejia <chis.mejia.garcia@gmail.com>
2020-08-10 21:48:00 -0500

    Primer Commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>CRUD</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8 mx-auto">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody>
+                 @foreach($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>Eliminar</td>
+                    </tr>
+                 @endforeach
+                </tbody>
+            </table>
+        </div> 
+    </div>
+</div>
+    
+</body>
+</html>
